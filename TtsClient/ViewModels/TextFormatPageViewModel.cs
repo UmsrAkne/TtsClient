2d78ba10ecb8c89a7846a13e525aefe122121d1c
UmsrAkne <sibata486@gmail.com>
2026-03-08 07:29:44 +0900

    feat: Add TextFormatPage and integrate into application
    - Introduced `TextFormatPage` view and its associated `TextFormatPageViewModel`.
    - Added `TextFormatPage` as a new tab in `MainWindow`.
    - Applied consistent styling for `TextBlock` and `TextBox` in `TextFormatPage`.

---
+++ TtsClient/ViewModels/TextFormatPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Mvvm;
+
+namespace TtsClient.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class TextFormatPageViewModel : BindableBase
+    {
+        
+    }
+}
