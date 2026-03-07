06dda09be428dd65f53349270768bdd103866e89
UmsrAkne <sibata486@gmail.com>
2026-03-07 19:29:09 +0900

    feat: Add debug data setup and refine ExplorerPage styles
    - Introduced `SetupDebugData()` method in `MainWindowViewModel` to populate test data in debug mode.
    - Enhanced `ExplorerPage.xaml` with consistent `FontSize` styling for `TextBlock` in `ListBox`.

--- TtsClient/ViewModels/MainWindowViewModel.cs
+++ TtsClient/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using System.Diagnostics;
+using Prism.Mvvm;
+using TtsClient.Models;
 using TtsClient.Utils;
 
 namespace TtsClient.ViewModels;
@@ -7,9 +9,26 @@ public class MainWindowViewModel : BindableBase
 {
     private readonly AppVersionInfo appVersionInfo = new ();
 
+    public MainWindowViewModel()
+    {
+        SetupDebugData();
+    }
+
     public string Title => appVersionInfo.Title;
 
     public EditorPageViewModel EditorPageViewModel { get; } = new ();
 
     public ExplorerPageViewModel ExplorerPageViewModel { get; } = new ();
+
+    [Conditional("DEBUG")]
+    private void SetupDebugData()
+    {
+        for (var i = 0; i < 50; i++)
+        {
+            ExplorerPageViewModel.SpeechEntries.Add(new SpeechEntry()
+            {
+                AudioPath = $"test_{i:000}.mp3",
+            });
+        }
+    }
 }
