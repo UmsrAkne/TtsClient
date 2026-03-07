1657025dfbbc1be6c961bfd1c821e0ea0904092f
UmsrAkne <sibata486@gmail.com>
2026-03-08 08:31:50 +0900

    feat: Add text processing workflow with debug data setup
    - Introduced `TextProcessingStep` and `ReplacementRule` classes to model text processing operations.
    - Updated `TextFormatPageViewModel` with support for processing steps and replacement rules.
    - Enhanced `TextFormatPage.xaml` to display and edit text processing steps and rules using a user-friendly UI.
    - Added `SetupDebugData` method for initializing debug data in development mode.
    - Applied consistent styling for controls and introduced `CommonBorderBrush` for improved visuals.

---
+++ TtsClient/Texts/TextProcessingStep.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Text.RegularExpressions;
+using Prism.Mvvm;
+
+namespace TtsClient.Texts
+{
+    public class TextProcessingStep : BindableBase
+    {
+        private string caption = string.Empty;
+        private string extractionPattern = string.Empty;
+
+        public string Caption { get => caption; set => SetProperty(ref caption, value); }
+
+        public string ExtractionPattern
+        {
+            get => extractionPattern;
+            set => SetProperty(ref extractionPattern, value);
+        }
+
+        public ObservableCollection<ReplacementRule> ReplacementRules { get; set; } = new ();
+
+        public string Execute(string text)
+        {
+            var matches = Regex.Matches(text, ExtractionPattern);
+
+            // まず抽出。複数引っかる場合は改行つき結合。
+            var builder = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                builder.AppendLine(match.Value);
+            }
+
+            var result = builder.ToString();
+
+            // 抽出したテキストに対して置き換え処理。フォーマットの仕上げ用途を想定。
+            foreach (var regexValue in ReplacementRules)
+            {
+                result = Regex.Replace(result, regexValue.Pattern, regexValue.Replacement);
+            }
+
+            return result;
+        }
+    }
+}
--- TtsClient/ViewModels/TextFormatPageViewModel.cs
+++ TtsClient/ViewModels/TextFormatPageViewModel.cs
@@ -1,10 +1,46 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Prism.Mvvm;
+using TtsClient.Texts;
 
 namespace TtsClient.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class TextFormatPageViewModel : BindableBase
     {
-        
+        private string originalText;
+
+        public TextFormatPageViewModel()
+        {
+            SetupDebugData();
+        }
+
+        public ObservableCollection<TextProcessingStep> TextProcessingSteps { get; } = new ();
+
+        public string OriginalText { get => originalText; set => SetProperty(ref originalText, value); }
+
+        [Conditional("DEBUG")]
+        private void SetupDebugData()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                var tp = new TextProcessingStep
+                {
+                    Caption = $"Header_{i}",
+                    ExtractionPattern = ".*",
+                };
+
+                for (int j = 0; j < i; j++)
+                {
+                    tp.ReplacementRules.Add(new ReplacementRule
+                    {
+                        Pattern = $"pattern_{j}",
+                        Replacement = "replacement",
+                    });
+                }
+
+                TextProcessingSteps.Add(tp);
+            }
+        }
     }
 }
