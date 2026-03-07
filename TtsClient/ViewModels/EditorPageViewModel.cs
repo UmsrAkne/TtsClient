000035e4b9d7e4a403143f60facca9fe7ef1bc97
UmsrAkne <sibata486@gmail.com>
2026-03-07 20:41:57 +0900

    feat: Add TTS request file saving and path helper utility
    - Updated `SendRequestCommand` in `EditorPageViewModel` to save synthesized audio as an MP3 file.
    - Added `PathHelper` utility for managing TTS audio directory paths.
    - Included audio file storage in a new "Audios" directory under the application base path.

--- TtsClient/ViewModels/EditorPageViewModel.cs
+++ TtsClient/ViewModels/EditorPageViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
 using CommunityToolkit.Mvvm.Input;
 using Prism.Mvvm;
 using TtsClient.TtsEngine;
+using TtsClient.Utils;
 
 namespace TtsClient.ViewModels
 {
@@ -17,7 +19,20 @@ namespace TtsClient.ViewModels
 
         public AsyncRelayCommand SendRequestCommand => new (async () =>
         {
-            await Task.Delay(1500);
+            var req = new TtsRequest
+            {
+                Text = "こんにちは、Google Cloud Text-to-Speechです。",
+                LanguageCode = "ja-JP",
+                Voice = "ja-JP-Wavenet-D",
+                Gender = "MALE",
+                AudioFormat = "MP3",
+            };
+
+            var byteArray = await ttsEngine.SynthesizeAsync(req);
+            var fileName = $"{Guid.NewGuid()}.mp3";
+            var path = Path.Combine(PathHelper.GetTtsAudioDirectoryPath(), fileName);
+
+            await File.WriteAllBytesAsync(path, byteArray);
         });
     }
 }
