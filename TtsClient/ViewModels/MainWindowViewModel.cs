6194df87d713c9b5f1d1d2017bd3d8d22c5b33e3
UmsrAkne <sibata486@gmail.com>
2026-03-12 17:51:10 +0900

    feat: Load speech history using `SpeechService` in `MainWindowViewModel`
    - Added `LoadSpeechesAsync` method to fetch speech history asynchronously.
    - Injected `SpeechService` into `MainWindowViewModel` constructor for dependency management.
    - Updated `ExplorerPageViewModel` to clear and populate speech entries with fetched data.

--- TtsClient/ViewModels/MainWindowViewModel.cs
+++ TtsClient/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Prism.Mvvm;
+using TtsClient.Databases;
 using TtsClient.Models;
 using TtsClient.Utils;
 
@@ -7,6 +10,7 @@ namespace TtsClient.ViewModels;
 
 public class MainWindowViewModel : BindableBase
 {
+    private readonly SpeechService speechService;
     private readonly AppVersionInfo appVersionInfo = new ();
 
     public MainWindowViewModel()
@@ -16,12 +20,17 @@ public class MainWindowViewModel : BindableBase
 
     public MainWindowViewModel(
         EditorPageViewModel editorPageViewModel,
-        ExplorerPageViewModel explorerPageViewModel)
+        ExplorerPageViewModel explorerPageViewModel,
+        SpeechService speechService)
     {
         Logger.Log("MainWindowViewModel constructor executed.");
+
+        this.speechService = speechService;
         EditorPageViewModel = editorPageViewModel;
         ExplorerPageViewModel = explorerPageViewModel;
         SetupDebugData();
+
+        _ = LoadSpeechesAsync();
     }
 
     public string Title => appVersionInfo.Title;
@@ -31,6 +40,24 @@ public class MainWindowViewModel : BindableBase
     public ExplorerPageViewModel ExplorerPageViewModel { get; }
 
     public TextFormatPageViewModel TextFormatPageViewModel { get; } = new ();
+
+    private async Task LoadSpeechesAsync()
+    {
+        ExplorerPageViewModel.SpeechEntries.Clear();
+
+        try
+        {
+            var data = await speechService.GetHistoryAsync();
+            foreach (var item in data)
+            {
+                ExplorerPageViewModel.SpeechEntries.Add(item);
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.Log(ex.ToString());
+        }
+    }
 
     [Conditional("DEBUG")]
     private void SetupDebugData()
